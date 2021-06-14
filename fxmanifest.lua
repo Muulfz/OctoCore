5650dd96d51f7fa5c4b3cfb86926f792cfe159c6
Matheus Rodrigues <mattreisro@gmail.com>
2021-06-15 00:53:20 +0100

    Octopus Framework core implementation

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'FastZ -  Muulfz'
+description 'Octopus Framework Core'
+version '1.0.0'
+
+
+server_script {
+    "__lib/utils.lua",
+    "__lib/required.lua",
+    'main.lua'
+}
